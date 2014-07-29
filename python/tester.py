b513f237128259b6981c7eaf40cbc9f0781a437f
sankethb <sankethb@umich.edu>
2014-07-29 19:46:16 -0400

    positions_latest
    with the actual delta and gamma eqns for counter 3.

---
+++ python/tester.py
@@ -0,0 +1,11 @@
+x = (0,100)
+a = (12,52)
+print a
+
+b= a[0]
+c= a[1]
+
+d = range(b,c)
+print d
+
+
---
+++ python/utils.py
@@ -0,0 +1,14 @@
+import time
+
+
+def getDateGMTFromUTCEpoch(epochs):
+	return time.strftime('%Y-%m-%d', time.gmtime(epochs/1000))
+
+
+def getTimeInSecs(timeStr):
+	time = map(lambda x: int(x),  timeStr.split(':'))
+	timeSecs = (time[0] * 24 * 60) + (time[1] * 60) + time[2]
+	return timeSecs
+
+def getInstrumentMetaStr(optionMeta):
+	return '%s - %.3f - %s' % (optionMeta.instrument, optionMeta.strike, getDateGMTFromUTCEpoch(optionMeta.expirationDate))
